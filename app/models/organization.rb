d3d5087d5a77687d73eda93c2197b32bc7b992a2
Caleb <calebcowen@gmail.com>
2016-08-17 14:01:43 -0600

    Fix failure in organization model test

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,5 @@
 class Organization < ActiveRecord::Base
   before_validation :generate_slug
-  validates_presence_of :slug
   has_many :recipients
   validates :name, presence: true, uniqueness: true
 
