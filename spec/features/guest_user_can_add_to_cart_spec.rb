bfbce3d04fde54b8e9d25a2024b188dde3026f3e
Lin <mccartneylc@gmail.com>
2016-08-17 18:11:46 -0600

    Fix recipient creation to accept id

--- spec/features/guest_user_can_add_to_cart_spec.rb
+++ spec/features/guest_user_can_add_to_cart_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "GuestUserCanAddToCart", type: :feature do
   scenario "they can select an amount to lend a recipient" do
   organization = Organization.create(name:"Homes for Humanity", description: "We build homes", status: 1)
-  organization.recipients.create(name:"Bob", description:"Loves building huts", country:"Ghana", image_path: "https://robohash.org/Bob")
+  country = Country.create(name:"Ghana")
+  organization.recipients.create(name:"Bob", description:"Loves building huts", country_id: country.id, image_path: "https://robohash.org/Bob")
+
 
   visit recipients_path
   expect(page).to have_content("Bob")
