1a4353bfbc4b16df1039b6c545a3ef65e678ab79
Angela Lindow <angelalindow@gmail.com>
2016-08-18 12:31:35 -0600

    update order controller to funding controller

--- app/controllers/funding/charges_controller.rb
+++ app/controllers/funding/charges_controller.rb
@@ -1,6 +1,6 @@
-class Order::ChargesController < Order::BaseController
+class Funding::ChargesController < Funding::BaseController
 
-  # 
+  #
   # def create
   # byebug
   #   # @order.update_attributes(cart_token: stripe_params["stripeToken"])
@@ -10,30 +10,28 @@ class Order::ChargesController < Order::BaseController
   #   @order = current_order
   #   flash[:notice] = "Order was successfully placed"
   # end
-  # # 
+  # #
   # private
-  # 
+  #
   # def stripe_params
   #   params.permit(:stripeToken)
   # end
   #   @amount = 500
-  # 
+  #
   #   customer = Stripe::Customer.create(
   #     :email => params[:stripeEmail],
   #     :source => params[:stripeToken]
   #   )
-  #   
+  #
   #   charge = Stripe::Charge.create(
   #     :customer => customer.id,
   #     :amount => @amount,
   #     :description => 'Rails Stripe customer',
   #     :currency => 'usd'
   #   )
-  #   
-  # rescue Stripe::CardError => e  
+  #
+  # rescue Stripe::CardError => e
   #   flash[:error] = e.message
-  #   redirect_to new_charge_path  
+  #   redirect_to new_charge_path
   # end
 end
-
-
