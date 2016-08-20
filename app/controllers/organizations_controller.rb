93da18baea550f3ac0636a94c92352697a0ca627
Tommasina <chompasina@users.noreply.github.com>
2016-08-20 12:25:58 -0600

    Guest user submits request for cause test isn't passing but we'… (#75)
    * [WIP] Guest user submits request for cause test isn't passing but we'll fix it in the morning
    
    * Test for guest user can submit a cause passes. Adds styling to login and register paths. Starts org admin index page.
    
    Approved by Lin and Caleb

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -16,7 +16,7 @@ class OrganizationsController < ApplicationController
   def create
     @organization = Organization.new(organization_params)
     if @organization.save
-      redirect_to root_path
+      redirect_to "/#{@organization.slug}/dashboard"
       flash[:success] = "You have submitted your organization application. We'll be in touch once we review it."
     else
       flash[:failure] = "Invalid Information"
@@ -27,8 +27,9 @@ class OrganizationsController < ApplicationController
   private
 
   def organization_params
-    params.require(:organization).permit(:name, :description, :image_path)
-    @organization = Organization.find_by(slug: params[:slug])
-    @recipients = @organization.recipients
+    params.require(:organization).permit(:name, :description, :image_path, :slug)
+    # @organization = Organization.find_by(slug: params[:slug])
+    # @recipients = @organization.recipients if @organization.recipients
   end
+  
 end
