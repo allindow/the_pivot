a7ec7ed9f36458e1fd15cd64b0b73bf48fe7ef72
Tommasina <tommasina1@gmail.com>
2016-08-21 11:22:43 -0600

    Adds org admin can login functionality. Test passing. Need to work on org admin dashboard next

---
+++ spec/features/org_admin_can_login_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Org admin visits homepage" do
+  scenario "org admin can login" do
+    
+    organization = Organization.create!(name:"Homes for Humanity", description: "We build homes", status: 1)
+    user = organization.users.create(username: 'fiona@cat.com', password: 'password')
+    role = Role.create(name: 'org_admin')
+    user.roles << role
+    
+    visit root_path
+    click_link 'Login'
+    fill_in 'Username', with: 'fiona@cat.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Login'
+    
+    expect(User.last.roles.pluck(:name)).to include('org_admin')
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Homes for Humanity")
+    expect(page).to have_link("Logout")
+    expect(page).to have_link("My Organization")
+  end
+end
