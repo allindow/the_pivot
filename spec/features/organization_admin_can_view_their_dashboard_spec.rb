e6c5ed4682715b7979107e48175f923db8ecb6cf
Lin <mccartneylc@gmail.com>
2016-08-21 12:26:23 -0600

    Add ability for user to view organization dashboard

--- spec/features/organization_admin_can_view_their_dashboard_spec.rb
+++ spec/features/organization_admin_can_view_their_dashboard_spec.rb
@@ -3,21 +3,27 @@ require 'rails_helper'
 RSpec.feature "Organization admin can view their org dashboard" do
   scenario "they can see their organization and link to manage team" do
 
-    organization = Organization.create(name:"Homes for Humanity", description: "We build homes", status: 1)
-    country = Country.create(name:"Ghana")
-    user = organization.users.create(name:"Bob", description:"Loves building huts", country_id: country.id, image_path: "https://robohash.org/Bob")
-    role = Role.create(name: "org_admin")
+    organization = Organization.create!(name:"Homes for Humanity", description: "We build homes", status: 1)
+    user = organization.users.create(username: 'fiona@cat.com', password: 'password')
+    role = Role.create(name: 'org_admin')
     user.roles << role
 
-    visit login_path
-    fill_in 'Username', with: 'samson@example.com'
+    visit root_path
+    click_link 'Login'
+    fill_in 'Username', with: 'fiona@cat.com'
     fill_in 'Password', with: 'password'
     click_button 'Login'
 
-    expect(page).to have_content("My Organization")
+    expect(User.last.roles.pluck(:name)).to include('org_admin')
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_link("My Organization")
 
     click_link "My Organization"
-    expect(page).to have_content("Homes of Humanity")
-    expect(page).to have_link("Manage Team")
+
+    expect(page).to have_content("Homes for Humanity")
+    expect(page).to have_content("Status: Active")
+    expect(page).to have_link("Manage Organization")
+    expect(page).to have_link("Manage Recipients")
+    expect(page).to have_link("Manage Admins")
   end
 end
