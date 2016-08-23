07c9e659ecd13bac0e57788298941607d07d6050
Caleb <calebcowen@gmail.com>
2016-08-23 12:28:38 -0600

    Removes learn more button from organization index/dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -69,8 +69,8 @@ ActiveRecord::Schema.define(version: 20160822055149) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_roles", force: :cascade do |t|
