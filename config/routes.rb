b0026b3ccd416f218dbe8f307486a5795d7248ee
Caleb9193 <calebcowen@yahoo.com>
2016-08-24 16:30:29 -0600

    Add platform admin can see all recipients sorted by organization (#135)

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   namespace :platform do
     resources :dashboard, only: [:index]
     resources :fundings, only: [:index]
+    resources :recipients, only: [:index]
     resources :organizations, only: [:index, :update, :edit]
     namespace :organization do
       resources :users, param: :organization_slug, only: [:new]
--- spec/features/registered_user/denied_registered_user_has_no_permissions_spec.rb
+++ spec/features/registered_user/denied_registered_user_has_no_permissions_spec.rb
@@ -6,19 +6,19 @@ RSpec.feature 'Registered user who has applied for an organization' do
     org = Organization.create!(name:'Hovels for Humanity', description: 'We build junky homes', status: 0)
     reg_user = org.users.create(username: 'omar@example.com', password: 'password')
     reg_user.roles << reg_role
-    
+
     visit login_path
-    
+
     fill_in 'Username', with: 'omar@example.com'
     fill_in 'Password', with: 'password'
     click_button 'Login'
-    
+
     expect(page).to have_content('Status: Pending')
-  
+    
     org.deny_organization_status
-    
+
     visit dashboard_path
-    
+
     expect(page).to have_content('Status: Denied')
     expect(page).to have_content('Please feel free to submit another organization application.')
     expect(page).to have_link('Register Organization')
