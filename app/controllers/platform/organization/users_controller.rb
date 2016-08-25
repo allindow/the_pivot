6d8f4ab5d228478b1d08f2fb893e20e2c27315b4
Angela Lindow <angelalindow@gmail.com>
2016-08-24 23:53:46 -0600

    Refactor platform organization users controller

--- app/controllers/platform/organization/users_controller.rb
+++ app/controllers/platform/organization/users_controller.rb
@@ -1,28 +1,37 @@
 class Platform::Organization::UsersController < ApplicationController
+  before_action :set_org, except: [:index, :create]
 
   def new
-    @organization = Organization.find_by(slug: params[:organization_slug])
     @user = @organization.users.new
   end
 
   def update
     @user = User.find_by(username: params[:user][:username])
-    @organization = Organization.find_by(slug: params[:organization_slug])
-    @role = Role.find_or_create_by(name: "org_admin")
-    if @user && @user.org_admin? == false
-      if current_user.org_admin? || current_user.platform_admin?
-        @user.roles << @role
-        @user.update!(organization_id: @organization.id)
-        flash[:success] = "You've added #{@user.username} as an admin"
-      end
+    if @user.not_org_admin?
+      promote_to_org_admin
       redirect_to platform_organizations_path
     elsif @user.org_admin?
-      flash[:failure] = "Could not add user"
-      redirect_to "/admin/#{@organization.slug}/users"
+      user_not_found
     else
       flash.now[:failure] = "Could not find user: #{params[:user][:username]}"
       render :new
     end
   end
 
+  private
+    def set_org
+      @organization = Organization.find_by(slug: params[:organization_slug])
+    end
+
+    def promote_to_org_admin
+      if current_user.org_admin? || current_user.platform_admin?
+        @user.promote_to_org_admin(@organization)
+        flash[:success] = "You've added #{@user.username} as an admin"
+      end
+    end
+
+    def user_not_found
+      flash[:failure] = "Could not add user"
+      redirect_to "/admin/#{@organization.slug}/users"
+    end
 end
