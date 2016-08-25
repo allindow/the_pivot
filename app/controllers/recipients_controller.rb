b9cec29b1107ea9d78f43f04571f0a94c0e552da
Angela Lindow <angelalindow@gmail.com>
2016-08-24 23:52:19 -0600

    Refactor recipients controller (#154)
    * Refactor and remove unused code from admin controllers and recipients controller

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,10 +1,11 @@
 class RecipientsController < ApplicationController
+  before_action :set_recipient, except: [:index, :create]
+
   def index
     @recipients = Recipient.active_only
   end
 
   def show
-    @recipient = Recipient.find(params[:id])
     if Recipient.active_only.include?(@recipient)
       @recipient
     else
@@ -14,40 +15,31 @@ class RecipientsController < ApplicationController
   end
 
   def create
-    country = Country.find_or_create_by(name: params[:recipient][:country])
-    organization = Organization.find(params[:organization_id])
-    if country.valid? && organization.valid?
-      @recipient = country.recipients.create(recipient_params.merge(organization: organization))
-      @recipient.image_path = "http://www.thecircle-net.com/images/costum/other/commingSoon.png" if params[:recipient][:image_path].blank?
-      if @recipient.save
-        redirect_to "/admin/#{@recipient.organization.slug}/recipients"
-        flash.now[:success] = "You have successfully added a recipient"
-      else
-        flash[:failure] = "Invalid information"
-        redirect_to "/#{organization.slug}/recipients/new"
-      end
+    @country = Country.find_or_create_by(name: params[:recipient][:country])
+    @organization = Organization.find(params[:organization_id])
+    if country_and_org_exist?
+      @recipient = @country.recipients.create(recipient_params.merge(organization: @organization))
+      check_for_image_path
+      check_if_recipient_saved
     else
       flash[:failure] = "Invalid information"
-      redirect_to "/#{organization.slug}/recipients/new"
+      redirect_to "/#{@organization.slug}/recipients/new"
     end
   end
 
   def destroy
-    recipient = Recipient.find(params[:id])
-    recipient.delete
+    @recipient.delete
     params.delete("id")
     redirect_to(:back)
   end
 
   def edit
-    @recipient = Recipient.find(params[:id])
   end
 
   def update
-    @recipient = Recipient.find(params[:id])
     @recipient.update_attributes(recipient_params)
-    @recipient.image_path = "http://www.thecircle-net.com/images/costum/other/commingSoon.png" if params[:recipient][:image_path].blank?
-    if @recipient.save
+    check_for_image_path
+     if @recipient.save
       redirect_to recipient_path
     else
       render :edit
@@ -56,8 +48,29 @@ class RecipientsController < ApplicationController
   end
 
   private
+    def set_recipient
+      @recipient = Recipient.find(params[:id])
+    end
 
     def recipient_params
       params.require(:recipient).permit(:name, :description, :image_path)
+    end
+
+    def country_and_org_exist?
+      @country.valid? && @organization.valid?
+    end
+
+    def check_for_image_path
+      @recipient.image_path = "http://www.thecircle-net.com/images/costum/other/commingSoon.png" if params[:recipient][:image_path].blank?
+    end
+
+    def check_if_recipient_saved
+      if @recipient.save
+        redirect_to "/admin/#{@recipient.organization.slug}/recipients"
+        flash.now[:success] = "You have successfully added a recipient"
+      else
+        flash[:failure] = "Invalid information"
+        redirect_to "/#{@organization.slug}/recipients/new"
+      end
     end
 end
