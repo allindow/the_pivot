30ccf41996660d91ce0052d7ee897b5beb4d171a
Caleb <calebcowen@gmail.com>
2016-08-25 03:19:44 -0600

    Adds test coverage to cart controller

--- spec/features/guest_user/user_can_delete_recipient_from_cart_spec.rb
+++ spec/features/guest_user/user_can_delete_recipient_from_cart_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "user can add and remove amount in cart" do
-  scenario "guest user adds recipient to cart and increases and decreases amount" do
+RSpec.feature "user can delete recipient from cart" do
+  scenario "guest user adds recipient to cart and deleted them" do
     organization = Organization.create(name:"Homes for Humanity", description: "We build homes", status: 1)
     country = Country.create(name:"Ghana")
     organization.recipients.create(name:"Bob", description:"Loves building huts", country_id: country.id, image_path: "https://robohash.org/Bob")
@@ -13,12 +13,11 @@ RSpec.feature "user can add and remove amount in cart" do
 
     visit cart_path
 
-    click_link "+"
-
-    expect(page).to have_content("Total: $30.00")
-
-    click_link "-"
-
+    expect(page).to have_content("Bob")
     expect(page).to have_content("Total: $20.00")
+
+    click_link "Remove"
+
+    expect(page).to have_content("Total: $0.00")
   end
 end
